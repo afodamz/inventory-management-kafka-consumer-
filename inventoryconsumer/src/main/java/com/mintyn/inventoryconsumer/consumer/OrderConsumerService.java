800f080ac166e614ca21f66b834f14f8ded6a361
afodamz <afodamz07@gmail.com>
2022-05-26 10:51:32 +0100

    calculate total price and get total items

--- inventoryconsumer/src/main/java/com/mintyn/inventoryconsumer/consumer/OrderConsumerService.java
+++ inventoryconsumer/src/main/java/com/mintyn/inventoryconsumer/consumer/OrderConsumerService.java
@@ -9,11 +9,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderConsumerService {
 
-    @KafkaListener(topics="orderProducts", groupId="order_json", containerFactory = "orderKafkaListenerFactory")
+    @KafkaListener(topics = "orderProducts", groupId = "order_json", containerFactory = "orderKafkaListenerFactory")
     public void consumeFromObject(Order message) {
-        System.out.println("Consumed message "+message);
+        System.out.println("Consumed message " + message);
 
         int totalItems = message.getItems().size();
+        int totalPrice = 0;
+        for (OrderItem item : message.getItems()){
+            int qunatity = item.getQuantity();
+            Double price = item.getProduct().getPrice();
+            Double itemPrice = price * price;
+            totalPrice = (int) (itemPrice + totalPrice);
+        }
 
     }
 }
